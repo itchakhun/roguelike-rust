97d0f982534b514848de154174d8bb1deae2b5ee
Itchakhun <16811182+itchakhun@users.noreply.github.com>
2021-09-04 08:03:02 +0700

    ♻️ derive

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
+#[derive(Component)]
 struct Position {
     x: i32,
     y: i32,
-}
-
-impl Component for Position {
-    type Storage = VecStorage<Self>;
 }
 
 struct State {}
